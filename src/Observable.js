12cad885ade892bc388c518cb33f2529b35815dc
László Kardinál <laszlo.kardinal@gmail.com>
2019-09-25 10:48:56 +0200

    wip

---
+++ src/Observable.js
@@ -0,0 +1,23 @@
+const Observable = {
+  create() {
+    let observers = [];
+
+    const emit = action => observers.forEach(observer => observer(action));
+
+    const subscribe = observer => {
+      const unsubscribe = () => {
+        observers = observers.filter(current => current !== observer);
+      };
+
+      unsubscribe(observer);
+
+      observers = observers.concat(observer);
+
+      return unsubscribe;
+    };
+
+    return { emit, subscribe };
+  }
+};
+
+export default Observable;
